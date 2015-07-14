839cdcba5e1aa43f93b09d79191c3fb2be97b221
Torey Hickman <torey@toreyhickman.com>
2015-07-14 16:46:07 -0500

    Remove database contraints

--- db/migrate/20140901164300_create_dogs.rb
+++ db/migrate/20140901164300_create_dogs.rb
@@ -1,8 +1,8 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
-      t.string   :name, { null: false, limit: 50 }
-      t.string   :license, { null: false }
+      t.string   :name
+      t.string   :license
       t.integer  :age
       t.integer  :weight
       t.integer  :owner_id
